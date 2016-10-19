6c57397134e8bee0bc33d13b056f1ccf97b86176
dhilipsiva <dhilipsiva@gmail.com>
2016-10-19 17:49:45 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ reqs = open("requirements.txt", "r").read().splitlines()
 
 setup(
     name='ak-vendor',
-    version='0.4.0',
+    version='0.4.1',
     description="Some vendor scripts that we use here at Appknox",
     long_description=long_description,
     url='https://github.com/appknox/vendor',
