91dea4aa6da4c6167acd688f93d2de2bd530fbca
yashwin <yashwinpoojary@gmail.com>
2017-04-06 11:57:53 +0530

    Minor update

--- ak_vendor/report.py
+++ ak_vendor/report.py
@@ -21,6 +21,7 @@ from django.template.engine import Engine
 from ak_vendor.utils import dict2obj
 from ak_vendor.seed import file_data
 from ak_vendor.constants import REPORT_PATH, OUTPUT_PATH
+from ak_vendor.enums import RiskEnum
 
 settings.configure(DEBUG=False)
 file = dict2obj(file_data)
@@ -30,7 +31,8 @@ content = ""
 with open(REPORT_PATH, "r") as input_file:
     template_string = input_file.read()
     template = Template(template_string, engine=Engine())
-    context = Context({"file": file})
+    context = Context(dict(
+        file=file, RiskEnum=RiskEnum))
     content = template.render(context)
 
 with open(OUTPUT_PATH, "w") as output_file:
