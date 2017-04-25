8b7acb1eb83d47c82696abaefe0ac99385c3615c
chillaranand <anand21nanda@gmail.com>
2017-04-25 16:08:54 +0530

    Bump version: 0.9.4 → 0.9.5

--- setup.py
+++ setup.py
@@ -1,23 +1,8 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# vim: fenc=utf-8
-# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
-#
-#
-
-"""
-File name: setup.py
-Version: 0.1
-Author: dhilipsiva <dhilipsiva@gmail.com>
-Date created: 2015-11-24
-"""
-
 from setuptools import setup, find_packages
 
 setup(
     name='ak-vendor',
-    version='0.9.4',
+    version='0.9.5',
     description="Some vendor scripts that we use here at Appknox",
     long_description="All the Vendor/helper files the Appknox relies on",
     url='https://github.com/appknox/vendor',
