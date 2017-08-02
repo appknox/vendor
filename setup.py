c729012b7fefedf6f364e0ed97db32ea3f4b6d79
dhilipsiva <dhilipsiva@gmail.com>
2017-08-02 19:12:02 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ak-vendor',
-    version='0.9.34',
+    version='0.9.35',
     description="Some vendor scripts that we use here at Appknox",
     long_description="All the Vendor/helper files the Appknox relies on",
     url='https://github.com/appknox/vendor',
