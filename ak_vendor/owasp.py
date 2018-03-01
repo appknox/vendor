02f4f58e5a73d3243a385c87a31b6a517a84b4e9
Sharat M R <cosmosgenius@gmail.com>
2018-03-01 16:32:10 +0530

    add owasp data

---
+++ ak_vendor/owasp.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+
+class OWASP():
+    def __init__(self, id, code, description, year):
+        self.id = id
+        self.code = code
+        self.description = description
+        self.year = year
+
+
+json_data = json.load(
+    open(os.path.join(os.path.dirname(__file__), 'owasp.json'))
+)
+data = {d['id']: OWASP(*d) for d in json_data}
