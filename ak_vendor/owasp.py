ffec5f36a69815bee3b3d74a024bf86cd255aec3
Sharat M R <cosmosgenius@gmail.com>
2018-03-06 14:53:15 +0530

    Remove owasp class only load json data

--- ak_vendor/owasp.py
+++ ak_vendor/owasp.py
@@ -1,17 +1,6 @@
 import json
 import os
 
-
-class OWASP():
-    def __init__(self, id, code, description, year):
-        self.pk = id
-        self.id = id
-        self.code = code
-        self.description = description
-        self.year = year
-
-
-json_data = json.load(
+data = json.load(
     open(os.path.join(os.path.dirname(__file__), 'owasp.json'))
 )
-data = {d['id']: OWASP(**d) for d in json_data}
