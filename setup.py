5cf19623bd766f8d9af4c42f1840dfdda27e065e
Praseetha-KR <praseetha04@gmail.com>
2018-10-09 19:19:48 +0530

    Upgraded dependency django 2.0.2 → 2.0.8

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     zip_safe=False,
     extras_require={
         'dev': [
-            'django==2.0.2',
+            'django==2.0.8',
             'weasyprint==0.42.2',
             'bumpversion==0.5.3',
             'twine==1.9.1'
