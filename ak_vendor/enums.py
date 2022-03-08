12f2f5dd57483d3d92721b7171e30ad3c8fcc34d
Ashraful Islam <avoid.ashraful@gmail.com>
2022-03-08 15:00:59 +0600

    Add enum class for FileType

--- ak_vendor/enums.py
+++ ak_vendor/enums.py
@@ -455,3 +455,15 @@ class MFAMethodEnum:
         NONE = [0, "None"]
         TOTP = [1, "TOTP"]
         HOTP = [2, "HOTP"]
+
+
+@choices
+class FileTypeEnum:
+    """
+    The file types.
+    """
+
+    class Meta:
+        ANDROID_APK = [0, "Android APK"]
+        ANDROID_AAB = [1, "Android AAB"]
+        IOS_IPA = [2, "IOS IPA"]
