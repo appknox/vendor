01f3e5d8033afe5f4c9afc76fad0cfacaa548b2d
Sharat M R <cosmosgenius@gmail.com>
2023-12-13 15:31:56 +0530

    generalize deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,35 @@
 #! /bin/bash
 #
 # deploy.sh
-# Copyright (C) 2015 dhilipsiva <dhilipsiva@gmail.com>
+# Copyright (C) 2023 Appknox <engineering@appknox.com>
 #
 # Distributed under terms of the MIT license.
 #
 
+in_array() {
+    local haystack="${1}[@]"
+    local needle=${2}
+    for i in ${!haystack}; do
+        if [[ ${i} == "${needle}" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
 
+# shellcheck disable=SC2034
+declare -a peotryversionpartarray=("major" "minor" "patch");
+
+if in_array peotryversionpartarray "$1"; then
+    echo "poetry version: $1"
+else
+    echo "ERROR: version is not one of major, minor, patch"
+    exit 1
+fi
+
+echo "Creating release"
 rm -rf dist/
-poetry version patch
+poetry version "$1"
 export CURRENT_BRANCH
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 VERSION=$(poetry version -s)
